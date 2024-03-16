c236b3e1c8cdb59a2e204736c5ed3adff855d635
dem1ess <stanislav_kharchenko@outlook.com>
2024-03-16 15:28:17 +0200

    update welcome

--- front-finance/src/components/Footer/Footer.tsx
+++ front-finance/src/components/Footer/Footer.tsx
@@ -1,59 +1,57 @@
 // components/Footer.tsx
 
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {MdDownload} from 'react-icons/md'
+import { MdDownload } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 export const Footer: React.FC = () => {
-    return (
-        <section className='fold mx-[-1.1vw] mb=[-1.1vw] is--footer'>
-            <div className='wrapp is--footer'>
-                <div className='footer-box'>
-                    <div className='footer-logo-side'>
-                        <div className='lang-logo'>
-                            <Link to='/' aria-current='page' className=''>
-                                <img className='h-[150px]' src='/TLD_logo_beige.svg' alt=''/>
-                            </Link>
-                        </div>
-                    </div>
-
-                </div>
-                <div className='footer-bottom'>
-                    <div>2024 © TLD Platform. All rights reserved</div>
-                    <div className='footer-bottom-box'>
-                        <a
-                            href="tel:+1234567890"
-                            className="button-with-icon is--no-shape textcolor-white w-inline-block">
-                            <div>{`+447436378629`}</div>
-                        </a>
-                        <a
-                            href='mailto:indo@tld-bali.com'
-                            className='is--no-shape textcolor-white w-inline-block'>
-                            info@tld-bali.com
-                        </a>
-                        <Link
-                            to="/privacy-policy"
-                            target="_blank"
-                            className="footer-link-bottom">
-                            Privacy Policy
-                        </Link>
-                        <a
-                            href="https://find-and-update.company-information.service.gov.uk/company/14791588/filing-history/MzM3NTM4OTAxOWFkaXF6a2N4/document?format=pdf&download=0"
-                            target='_blank'
-                            className='flex mt-0.5 footer-link-bottom'>
-                            Company license
-                            <MdDownload className='text-lg'/>
-                        </a>
-                        <Link
-                            to="/terms-of-service"
-                            target="_blank"
-                            className="footer-link-bottom">
-                            Terms of Service
-                        </Link>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section className='fold mx-[-1.1vw] mb=[-1.1vw] is--footer'>
+			<div className='wrapp is--footer'>
+				<div className='footer-box'>
+					<div className='footer-logo-side'>
+						<div className='lang-logo justify-center items-center'>
+							<Link to='/' aria-current='page' className=''>
+								<img className='h-[150px]' src='/TLD_logo_beige.svg' alt='' />
+							</Link>
+						</div>
+					</div>
+				</div>
+				<div className='footer-bottom'>
+					<div>2024 © TLD Platform. All rights reserved</div>
+					<div className='footer-bottom-box'>
+						<a
+							href='tel:+1234567890'
+							className='button-with-icon is--no-shape textcolor-white w-inline-block'>
+							<div>{`+447436378629`}</div>
+						</a>
+						<a
+							href='mailto:indo@tld-bali.com'
+							className='is--no-shape textcolor-white w-inline-block'>
+							info@tld-bali.com
+						</a>
+						<Link
+							to='/privacy-policy'
+							target='_blank'
+							className='footer-link-bottom'>
+							Privacy Policy
+						</Link>
+						<a
+							href='https://find-and-update.company-information.service.gov.uk/company/14791588/filing-history/MzM3NTM4OTAxOWFkaXF6a2N4/document?format=pdf&download=0'
+							target='_blank'
+							className='flex mt-0.5 footer-link-bottom'>
+							Company license
+							<MdDownload className='text-lg' />
+						</a>
+						<Link
+							to='/terms-of-service'
+							target='_blank'
+							className='footer-link-bottom'>
+							Terms of Service
+						</Link>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
--- front-finance/src/components/SectionApartTabs/ApartItems.tsx
+++ front-finance/src/components/SectionApartTabs/ApartItems.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { IProperty } from '../../models/IProperty.ts'
 import { Link } from 'react-router-dom'
+import { IProperty } from '../../models/IProperty.ts'
 
 interface Iprops {
 	property: IProperty
@@ -11,36 +11,40 @@ const ApartmentItem: React.FC<Iprops> = ({ property }) => {
 		<div
 			id={property.id}
 			role='group'
-			className='apartments-item rounded-xl shadow-small overflow-hidden border-4 border-color--border min-h-[510px] min-w-[350.333px] max-h-[900px] max-w-[387.333px]'>
+			className='apartments-item rounded-xl shadow-small overflow-hidden border-4 border-color--border min-h-[510px] max-w-[387.333px] sm:min-w-[350.333px] sm:max-h-[900px] md:min-w-[300px] md:max-h-[800px] lg:min-w-[250px] lg:max-h-[700px] xl:min-w-[200px] xl:max-h-[600px]'>
 			<Link
 				to={`/asset/${property.id}`}
 				className='itme-link-wrapper w-inline-block'>
 				<div className='apart-cover'>
-					<img src={`/villas/${property.photoUrls[0]}`} alt={property.description} className='apart-cover-image' />
+					<img
+						src={`/villas/${property.photoUrls[0]}`}
+						alt={property.description}
+						className='apart-cover-image'
+					/>
 				</div>
-				<div className="apart-title">
-					<div className="price-investors items-center justify-center">
-						<div className="newtext-24-bold-120-3 text-white">{property.name}</div>
+				<div className='apart-title'>
+					<div className='price-investors items-center justify-center'>
+						<div className='newtext-24-bold-120-3 text-white'>
+							{property.name}
+						</div>
 					</div>
-					<div className="apart-currency-number text-gray-300">
-						<div className="newtext-18-bold-120-2">$</div>
-						<div className="newtext-18-bold-120-2">{property.price.toLocaleString()}</div>
+					<div className='apart-currency-number text-gray-300'>
+						<div className='newtext-18-bold-120-2'>$</div>
+						<div className='newtext-18-bold-120-2'>
+							{property.price.toLocaleString()}
+						</div>
 					</div>
 				</div>
 
-				<div className="apart-percents">
-					<div className="apart-percents-growth">
-						<div className='text-xl text-gray-100'>
-							Year of commissioning
-						</div>
-						<div className="newtext-16-regular-150 text-gray-200">
+				<div className='apart-percents'>
+					<div className='apart-percents-growth'>
+						<div className='text-xl text-gray-100'>Year of commissioning</div>
+						<div className='newtext-16-regular-150 text-gray-200'>
 							{property.yearOfCompletion}
 						</div>
 					</div>
 					<div className='apart-percents-return'>
-						<div className='text-xl text-gray-100'>
-							ROI
-						</div>
+						<div className='text-xl text-gray-100'>ROI</div>
 						<div className='newtext-16-regular-150 text-gray-200'>
 							{property.roi}%
 						</div>
