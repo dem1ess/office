7555457ad9956aaacbc07ca1bd48ec8409012852
dem1ess <stanislav_kharchenko@outlook.com>
2024-03-19 16:52:30 +0200

    asdsa

--- back-finance/src/transaction/dto/prmoney-callback.dto.ts
+++ back-finance/src/transaction/dto/prmoney-callback.dto.ts
@@ -1,38 +1,13 @@
-import { IsString } from 'class-validator'
-// dto/prmoney-callback.dto.ts
-import { IsNumber } from 'class-validator'
-
 export class PrMoneyCallbackDto {
-  @IsString()
-  order_id: string
-
-  @IsString()
-  id: string
-
-  @IsString()
-  status: string
-
-  @IsNumber()
-  amount: number
-
-  @IsNumber()
-  amount_fee: number
-
-  @IsNumber()
-  actual_amount: number
-
-  @IsString()
-  client_id: string
-
-  @IsString()
-  description: string
-
-  @IsString()
-  card: string
-
-  @IsString()
-  comment: string
-
-  @IsString()
-  signature: string
+  order_id: any
+  id: any
+  status: any
+  amount: any
+  amount_fee: any
+  actual_amount: any
+  client_id: any
+  description: any
+  card: any
+  comment: any
+  signature: any
 }
